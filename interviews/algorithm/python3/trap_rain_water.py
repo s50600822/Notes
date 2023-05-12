c702a2749b0db5e5392758efd05d9cdbca87ac89
HoaPhan <s50600822@gmail.com>
2023-05-13 07:06:42 +1000

    fix

--- interviews/algorithm/python3/trap_rain_water.py
+++ interviews/algorithm/python3/trap_rain_water.py
@@ -8,7 +8,7 @@ def trap(height):
     right_max[n-1] = height[n-1]
     for i in range(1, n):
         left_max[i] = max(left_max[i-1], height[i])
-    for i in range(n-1, -1, -1):
+    for i in range(n-2, -1, -1):
         right_max[i] = max(right_max[i+1], height[i])
     total_water = 0
     for i in range(n):
