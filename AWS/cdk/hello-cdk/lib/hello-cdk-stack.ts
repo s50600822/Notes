0b14395a0680b1e21364309a7264bf8d7590bd15
s50600822 <s50600822@gmail.com>
2023-06-03 00:17:24 +1000

    sqs

--- AWS/cdk/hello-cdk/lib/hello-cdk-stack.ts
+++ AWS/cdk/hello-cdk/lib/hello-cdk-stack.ts
@@ -1,12 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true
+    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
     });
+
+    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
