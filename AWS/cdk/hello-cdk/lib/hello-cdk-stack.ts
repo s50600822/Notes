445773358a90fd784ac41b4bae41b6dd0e03ed63
s50600822 <s50600822@gmail.com>
2023-06-16 08:25:56 +1000

    sec

--- AWS/cdk/hello-cdk/lib/hello-cdk-stack.ts
+++ AWS/cdk/hello-cdk/lib/hello-cdk-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -9,5 +12,22 @@ export class HelloCdkStack extends cdk.Stack {
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
       handler: 'hello.handler'                // file is "hello", function is "handler"
     });
+
+    const secret = new secretsmanager.Secret(this, 'FamilySec', {
+      secretName: 'Nflx',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: 'myNetFlixUser' }),
+        generateStringKey: 'netflixPass',
+        excludePunctuation: true,
+      },
+    });
+
+    const role = new iam.Role(this, 'Family', {
+      assumedBy: new iam.AccountRootPrincipal(),
+    });
+
+    secret.grantRead(role); 
+    secret.grantWrite(role);
+
   }
 }
