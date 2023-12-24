a5010989396a158f12798ae7680536fc5529495b
HoaPhan <s50600822@gmail.com>
2023-12-24 16:17:51 +1100

    https://leetcode.com/problems/set-matrix-zeroes/ note

--- interview_prep/algorithm/java/ide_handicapped/set_matrix_zeroes/Solution.java
+++ interview_prep/algorithm/java/ide_handicapped/set_matrix_zeroes/Solution.java
@@ -39,4 +39,81 @@ class Solution {
         self.setZeroes(in);
         assert Arrays.deepEquals(in, out) == true;
     }
+
+    public void setZeroesBetterSpace(int[][] matrix) {
+        // using first row and col as marker instead of using separated array
+        // special note to the first element
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        // use first row and first column as marker
+        // first row/col itself 0 or not? use a boolean flag
+        boolean isFirstRowZero = false;
+        boolean isFirstColZero = false;
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                isFirstColZero = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        // set 0: rest first, first-row/col last
+        for (int col = 1; col < n; col++) {
+            if (matrix[0][col] == 0) {
+
+                int i = 1;
+                while (i < m) {
+                    matrix[i][col] = 0;
+                    i++;
+                }
+            }
+        }
+        for (int row = 1; row < m; row++) {
+            if (matrix[row][0] == 0) {
+
+                int j = 1;
+                while (j < n) {
+                    matrix[row][j] = 0;
+                    j++;
+                }
+            }
+        }
+
+        if (isFirstRowZero) {
+            int j = 0;
+            while (j < n) {
+                matrix[0][j] = 0;
+                j++;
+            }
+        }
+        if (isFirstColZero) {
+            int i = 0;
+            while (i < m) {
+                matrix[i][0] = 0;
+                i++;
+            }
+        }
+    }
 }
